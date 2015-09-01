dcb34c8fe1f95b9a3f79e3158bb3b98a560ea709
Thomas Franquelin <ostapneko@gmail.com>
2015-09-01 17:46:15 +0100

    Refactor the Zuora service

---
+++ src/main/scala/com/gu/subscriptions/cas/model/Implicits.scala
@@ -0,0 +1,13 @@
+package com.gu.subscriptions.cas.model
+
+import com.gu.membership.zuora.soap.Zuora.Contact
+
+object Implicits {
+  implicit class ContactOpts(contact: Contact) {
+    def samePostcode(otherPostalCode: String): Boolean = {
+      def format(pc: String): String = pc.replaceAll("\\s+", "").toLowerCase
+
+      format(contact.postalCode) == format(otherPostalCode)
+    }
+  }
+}
