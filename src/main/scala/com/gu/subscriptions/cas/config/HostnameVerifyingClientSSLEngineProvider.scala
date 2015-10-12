9ed651a346acdb0f2ed36eb8ac0461a8bea289fc
tverran <tom.verran@guardian.co.uk>
2015-10-12 17:20:47 +0100

    omit some dots and parentheses

--- src/main/scala/com/gu/subscriptions/cas/config/HostnameVerifyingClientSSLEngineProvider.scala
+++ src/main/scala/com/gu/subscriptions/cas/config/HostnameVerifyingClientSSLEngineProvider.scala
@@ -5,8 +5,8 @@ import spray.io.ClientSSLEngineProvider
 object HostnameVerifyingClientSSLEngineProvider {
     implicit val provider = ClientSSLEngineProvider { engine =>
         val p: SSLParameters = engine.getSSLParameters
-        p.setEndpointIdentificationAlgorithm("HTTPS")
-        engine.setSSLParameters(p)
+        p setEndpointIdentificationAlgorithm "HTTPS"
+        engine setSSLParameters p
         engine
     }
 }
