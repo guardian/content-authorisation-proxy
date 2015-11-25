9ef717c075dc02959c6dcfb0bb1ad2b96e9ce03d
Chris J Owen <chris.j.owen@guardian.co.uk>
2015-11-25 18:27:13 +0000

    Subs proxied request will now make calls to both the old and new instances and log if the responses are different

--- src/main/scala/com/gu/subscriptions/cas/directives/ProxyDirective.scala
+++ src/main/scala/com/gu/subscriptions/cas/directives/ProxyDirective.scala
@@ -13,10 +13,11 @@ import com.gu.subscriptions.cas.model.{SubscriptionExpiration, SubscriptionReque
 import com.gu.subscriptions.cas.monitoring.{RequestMetrics, StatusMetrics}
 import com.gu.subscriptions.cas.service.SubscriptionService
 import com.gu.subscriptions.cas.service.zuora.ZuoraSubscriptionService
+import com.typesafe.scalalogging.LazyLogging
 import spray.can.Http
 import spray.can.Http.HostConnectorSetup
 import spray.http.HttpHeaders._
-import spray.http.{HttpRequest, HttpResponse}
+import spray.http.{HttpRequest, HttpResponse, Uri}
 import spray.httpx.ResponseTransformation._
 import spray.httpx.SprayJsonSupport._
 import spray.routing.{Directives, Route}
@@ -25,27 +26,38 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
-trait ProxyDirective extends Directives with ErrorRoute {
+trait ProxyDirective extends Directives with ErrorRoute with LazyLogging {
 
   implicit val actorSystem: ActorSystem
-  implicit val timeout: Timeout = 1.seconds
-
+  implicit val timeout: Timeout = 2.seconds
   lazy val io: ActorRef = IO(Http)
   lazy val subscriptionService: SubscriptionService = ZuoraSubscriptionService
-  lazy val proxyHost = Configuration.proxyHost
-  lazy val proxyPort = Configuration.proxyPort
-  lazy val proxyScheme = Configuration.proxyScheme
 
-  def proxyRequest(in: HttpRequest): HttpRequest =
-    in.copy(
-      uri = in.uri.withHost(proxyHost).withPort(proxyPort).withScheme(proxyScheme),
+
+  def proxyRequest(in: HttpRequest, proxy: String): Future[HttpResponse] = {
+    val metrics = new CASMetrics(Configuration.stage)
+    val (proxyScheme, proxyHost, proxyPort, proxyPath) = {
+      val proxyUri = Uri(proxy)
+      (proxyUri.scheme, proxyUri.authority.host.address, proxyUri.effectivePort, proxyUri.path)
+    }
+
+    val request = in.copy(
+      uri = in.uri.withHost(proxyHost).withPort(proxyPort).withScheme(proxyScheme).withPath(Uri.Path(proxyPath.toString() + in.uri.path.toString())),
       headers = in.headers.map {
         case Host(_, _) => Host(proxyHost)
         case header => header
       }
     )
+    val hostConnectorSetup = HostConnectorSetup(proxyHost, proxyPort, sslEncryption = true)
+    (io ?(request, hostConnectorSetup)).mapTo[HttpResponse].map(
+      logProxyResp(metrics) ~>
+        filterHeaders ~>
+        changeResponseCode
+    )
+  }
 
   def logProxyResp(metrics: CASMetrics): HttpResponse => HttpResponse = { resp =>
+    metrics.putRequest
     metrics.putResponseCode(resp.status.intValue, "POST")
     resp
   }
@@ -56,27 +68,47 @@ trait ProxyDirective extends Directives with ErrorRoute {
       case _ => true
     })
 
-  def hostConnectorSetup = HostConnectorSetup(proxyHost, proxyPort, sslEncryption = true)
+  private def compareSubscriptionRequests(request: HttpRequest, casResponse: Future[HttpResponse]): Unit = {
+    val uri: Uri = request.uri
+    val isSubsRequest = uri.path != null &&
+      uri.path.toString().contains("/subs")
 
-  def sendReceive(request: HttpRequest, metrics: CASMetrics, followRedirect: Boolean = true): Future[HttpResponse] = {
-    (io ? (request, hostConnectorSetup)).mapTo[HttpResponse].map(
-      logProxyResp(metrics) ~>
-          filterHeaders ~>
-          changeResponseCode
-    )
+    def compareResponses(oldResponse: HttpResponse, newResponse: HttpResponse) {
+      val hasSameStatus = oldResponse.status == newResponse.status
+      val hasSameBody = oldResponse.entity.asString == newResponse.entity.asString
+      if (!hasSameStatus && !hasSameBody)
+        logger.error("Legacy apps returned different Responses")
+      else
+        logger.info("Legacy apps returned same Responses ")
+    }
+
+    if (isSubsRequest) {
+      logger.info("Subs request made, comparing old with new CAS instances")
+      for {
+        oldResponse <- casResponse
+        newResponse <- proxyRequest(request, Configuration.proxyNew)
+      } yield compareResponses(oldResponse, newResponse)
+    }
   }
 
   lazy val casRoute: Route = {
-    val metrics = new CASMetrics(Configuration.stage)
-    ctx => ctx.complete(sendReceive(proxyRequest(ctx.request), metrics))
+    ctx => {
+      val request: HttpRequest = ctx.request
+      val casResponse = proxyRequest(request, Configuration.proxy)
+      ctx.complete(casResponse)
+      try {
+        compareSubscriptionRequests(request, casResponse)
+      }
+      catch {
+        case e: Exception => logger.error("Error comparing SubscriptionRequests", e)
+      }
+    }
   }
 
   val authRoute: Route = (path("auth") & post)(casRoute)
 
   def zuoraRoute(subsReq: SubscriptionRequest): Route = zuoraDirective(subsReq) { subscriptionName =>
-
     val validSubscription = subscriptionService.getValidSubscription(subscriptionName, subsReq.password)
-
     onSuccess(validSubscription) {
       case Some(subscription) =>
         subscriptionService.updateActivationDate(subscription)
