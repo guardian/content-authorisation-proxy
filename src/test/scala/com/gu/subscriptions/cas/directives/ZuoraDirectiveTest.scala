0421ec2d4560ddf6ca030711919d84d2cf5abd03
Thomas Franquelin <ostapneko@gmail.com>
2016-01-20 15:55:29 +0000

    Do not fill in the ActivationDate when there is a noActivation query parameter

--- src/test/scala/com/gu/subscriptions/cas/directives/ZuoraDirectiveTest.scala
+++ src/test/scala/com/gu/subscriptions/cas/directives/ZuoraDirectiveTest.scala
@@ -3,6 +3,7 @@ package com.gu.subscriptions.cas.directives
 
 import com.gu.subscriptions.cas.model.SubscriptionRequest
 import org.scalatest.{FlatSpec, Matchers}
+import spray.http.Uri
 import spray.routing.{HttpService, Route}
 import spray.testkit.ScalatestRouteTest
 
@@ -15,7 +16,7 @@ class ZuoraDirectiveTest extends FlatSpec with Matchers with ScalatestRouteTest 
 
   "A SubscriptionRequest" should "pass if subscriberId starts with A-S" in {
     val subscriptionRequest = new SubscriptionRequest(Some("A-S00056789"), "password")
-    val route: Route = ZuoraDirective.zuoraDirective(subscriptionRequest) { subId =>
+    val route: Route = ZuoraDirective.zuoraDirective(subscriptionRequest) { (triggersActivation, subId) =>
       complete(subId)
     }
     Get("/") ~> route ~> check {
@@ -25,11 +26,27 @@ class ZuoraDirectiveTest extends FlatSpec with Matchers with ScalatestRouteTest 
 
   "A SubscriptionRequest" should "be rejected if the subscriberId does not start with A-S" in {
     val subscriptionRequest = new SubscriptionRequest(Some("100056789"), "password")
-    val route: Route = ZuoraDirective.zuoraDirective(subscriptionRequest) { subId =>
+    val route: Route = ZuoraDirective.zuoraDirective(subscriptionRequest) { (triggersActivation, subId) =>
       complete(subId)
     }
     Get("/") ~> route ~> check {
       assert(!handled)
+    }
+  }
+
+  "A SubscriptionRequest" should "return a true triggersActivation value unless given a noActivation query parameter" in {
+    val subscriptionRequest = new SubscriptionRequest(Some("A-S00056789"), "password")
+    val route: Route = ZuoraDirective.zuoraDirective(subscriptionRequest) { (triggersActivation, subId) =>
+      val activation = if (triggersActivation) "true" else "false"
+      complete(activation)
+    }
+
+    Get("/") ~> route ~> check {
+      responseAs[String] should be("true")
+    }
+
+    Get(Uri("/").withQuery(("noActivation", "true"))) ~> route ~> check {
+      responseAs[String] should be("false")
     }
   }
 
